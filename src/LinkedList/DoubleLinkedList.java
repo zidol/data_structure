5f4b562904a9caba52bf044274862210ae410420
zidol <ajh3166@naver.com>
2022-03-16 00:23:52 +0900

    5-1. 더블 연결 리스트 코드 수정 및 주석 추가

--- src/LinkedList/DoubleLinkedList.java
+++ src/LinkedList/DoubleLinkedList.java
@@ -24,7 +24,7 @@ public class DoubleLinkedList<T> {
                 node = node.next;
             }
             node.next = new Node<T>(data);
-            node.prev = node; //현재 노드를 지정
+            node.next.prev = node; //현재 노드를 지정
             this.tail = node.next;
         }
     }
@@ -52,8 +52,8 @@ public class DoubleLinkedList<T> {
                     node = node.next;
                 }
             }
+            return null;
         }
-        return null;
     }
 
     public T searchFromTail(T isData) {
@@ -82,15 +82,17 @@ public class DoubleLinkedList<T> {
             Node<T> newHead = new Node<T>(addData);
             newHead.next = this.head; //새로 넣은 데이터 next에 기존 헤드를 연결
             this.head = newHead;      //헤드에 새로운 값을 넣음
-            this.head.next.prev = this.head;
+            this.head.next.prev = this.head; // 헤드 다음 노드의 prev에 head를 연결 해줌
             return true;
-        } else {
+        } else {//헤드가 아닌 그 이후에 연결된 노드 중 하나 일때
             Node<T> node = this.head;
             while (node != null) {
                 if (node.data == exitedData) {
-                    Node<T> nodePrev = node.prev;   //현재 노드의 prev
-                    nodePrev.next = new Node<T>(addData);   //현재 노드의 prev의 next에 새로 넣을 값을 연결
+                    Node<T> nodePrev = node.prev;   //현재 노드의 앞에 있는 노드(nodePrev)
+
+                    nodePrev.next = new Node<T>(addData);   //현재 노드의 앞에 있는 노드(nodePrev)의 next에 새로 넣을 노드를 연결
                     nodePrev.next.next = node;  //추가한 addData의 next에  현재 노드 데이터를 연결
+
                     nodePrev.next.prev = nodePrev;  //추가한 addData의 prev는 null 이기때문에 nodePrev를 추가해줘야
                     node.prev = nodePrev.next; //현재 노드의 prev는 addData(nodePrev.next)를 연결해야함
                     return true;
@@ -118,14 +120,15 @@ public class DoubleLinkedList<T> {
         System.out.println("---------------");
         System.out.println(linkedList.insertFrontData(3, 2));
         linkedList.printAll();
-        System.out.println("---------------");
+        System.out.println("----------------");
 
-        System.out.println(linkedList.insertFrontData(6, 2));
-        System.out.println(linkedList.insertFrontData(1, 0));
+        linkedList.insertFrontData(6, 2);
+        linkedList.insertFrontData(1, 0);
         linkedList.printAll();
-        System.out.println("---------------");
+        System.out.println("----------------");
 
-        linkedList.addNode(7);
+        linkedList.addNode(6);
         linkedList.printAll();
+
     }
 }
--- src/LinkedList/Readme.md
+++ src/LinkedList/Readme.md
@@ -6,7 +6,7 @@
 
 <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbqsySc%2FbtqEk1stewE%2Ftnty2kV69c7l45eyUO3Jh0%2Fimg.png"/>
 
-### 2. 링크드 리스트의 장단점 (전통적인 C언어에서의 배열과 링크드 리스트)
+## 2. 링크드 리스트의 장단점 (전통적인 C언어에서의 배열과 링크드 리스트)
 * 장점
     - 미리 데이터 공간을 미리 할당하지 않아도 됨
         - 배열은 **미리 데이터 공간을 할당** 해야 함
@@ -14,3 +14,17 @@
     - 연결을 위한 별도 데이터 공간이 필요하므로, 저장공간 효율이 높지 않음
     - 연결 정보를 찾는 시간이 필요하므로 접근 속도가 느림
     - 중간 데이터 삭제시, 앞뒤 데이터의 연결을 재구성해야 하는 부가적인 작업 필요
+### 3. 링크드 리스트의 복잡한 기능1 (링크드 리스트 데이터 사이에 데이터를 추가)
+- 링크드 리스트는 유지 관리에 부가적인 구현이 필요함
+
+<img src="https://www.fun-coding.org/00_Images/linkedlistadd.png" />
+
+> 출처: wikipedia, https://en.wikipedia.org/wiki/Linked_list
+### 4. 다양한 링크드 리스트 구조: 더블 링크드 리스트(Doubly linked list)
+* 더블 링크드 리스트(Doubly linked list) 기본 구조
+    - 이중 연결 리스트라고도 함
+    - 장점: 양방향으로 연결되어 있어서 노드 탐색이 양쪽으로 모두 가능
+      <br>
+      <br>
+      <img src="https://www.fun-coding.org/00_Images/doublelinkedlist.png" />
+> 출처: wikipedia, https://en.wikipedia.org/wiki/Linked_list)
