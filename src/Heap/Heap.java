f9766980d40952560577669b4587d026db1a0467
zidol <ajh3166@naver.com>
2022-03-30 22:42:47 +0900

    8-2. Heap 데이터 입력

--- src/Heap/Heap.java
+++ src/Heap/Heap.java
@@ -1,6 +1,7 @@
 package Heap;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Heap {
     public ArrayList<Integer> heapArray = null;
@@ -13,14 +14,48 @@ public class Heap {
     }
 
     public boolean insert(Integer data) {
+        Integer insertedIdx, parentIdx;
         if (heapArray == null) {
             heapArray = new ArrayList<Integer>();
 
             heapArray.add(null);
             heapArray.add(data);
-        } else {
-            heapArray.add(data);
+            return true;
+        }
+
+        this.heapArray.add(data);
+        //마지막 노드
+        insertedIdx = this.heapArray.size() - 1;
+        while (this.moveUp(insertedIdx)) {
+            parentIdx = insertedIdx / 2;
+            Collections.swap(this.heapArray, insertedIdx, parentIdx);
+            insertedIdx = parentIdx;
         }
         return true;
+    }
+
+    public boolean moveUp(Integer insertedIdx) {
+        //root node 인지 체크
+        if (insertedIdx <= 1) {
+            //더 이상 부모 노드 없음...
+            return false;
+        }
+        Integer parentIdx = insertedIdx / 2;
+        if (this.heapArray.get(insertedIdx) > this.heapArray.get(parentIdx)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Heap heap = new Heap(15);
+        heap.insert(10);
+        heap.insert(8);
+        heap.insert(5);
+        heap.insert(4);
+        heap.insert(20);
+
+        System.out.println(heap.heapArray);
     }
 }
